3bbcd7f6507a2fb3718c35cb68bfdf634485f816
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-09-21 19:41:52 -0300

    refactor: Se adecua modelo para incorporar el handle

---
+++ src/models/user.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export interface IUser {
+    handle: string;
+    name: string;
+    email: string;
+    password: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const userSchema = new mongoose.Schema({
+    handle: { type: String, required: true, trim: true, lowercase: true, unique: true},
+    name: { type: String, required: true, trim: true, },
+    email: { type: String, required: true, unique: true, trim: true, lowercase:true},
+    password: { type: String, required: true, trim: true, },
+}, { timestamps: true });
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
